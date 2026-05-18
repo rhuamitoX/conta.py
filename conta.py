36b464592dfa170caa246348dbc5299da20cbcaf
Rhuan Filipe da Silva <filipe.silva.rhuan@escola.pr.gov.br>
2026-05-18 12:22:06 -0300

    Add files via upload

--- conta.py
+++ conta.py
@@ -1,20 +1,48 @@
 import random
-
 class Conta:
     # Método construtor: cria um objeto conforme a necessidade do usuário
     def __init__(self,titular,cpf,limite,chave_pix, senha):
         # Atributos:
-        self.titular = titular
-        self.cpf = cpf
-        self.agencia = 2026
-        self.conta = random.randint(100000,999999)
-        self.saldo = 0
-        self.limite = limite
-        self.chave_pix = chave_pix
-        self.senha = senha
-
+        self.__titular = titular
+        self.__cpf = cpf
+        self.__agencia = 2026
+        self.__conta = random.randint(100000,999999)
+        self.__saldo = 0
+        self.__limite = limite
+        self.__chave_pix = chave_pix
+        self.__senha = senha
         
-    def mostrar(self):
-        print(f'''Titular: {self.titular}
-                 \nAgência: {self.agencia} 
-                 \nConta: {self.conta}''')                           
+    # Encapsulamento (getters e setters)
+    # getter do titular
+    @property
+    def titular(self):
+        return self.__titular
+    #setter do titular
+    @titular.setter
+    def titular(self, novo_nome):
+        self.__titular = novo_nome
+
+    # métodos da classe
+    def extrato(self):
+        print(f'''Titular: {self.__titular}
+                 \nAgência: {self.__agencia} 
+                 \nConta: {self.__conta}
+                 \nSaldo: {self.__saldo}''')
+
+    def sacar(self,valor):
+        if valor <= self.__saldo and valor > 0:
+            self.__saldo -= valor
+            print("Saque efetuado com sucesso!")
+        else:
+            print("Não foi possível efetuar o saque! Verifique seu saldo.")
+
+    def depositar(self, valor):
+        if valor > 0:
+            self.__saldo += valor
+            print("Depósito efetuado com sucesso!")
+        else:
+            print("Não foi possivel realizar o depósito.")
+
+    def transferir(self, valor, conta_destino):
+         self.__sacar(valor)
+         conta_destino.depositar(valor)                
---
+++ text.py
@@ -0,0 +1,6 @@
+from conta import Conta
+conta1 = Conta("rhuan",65478096920, 1000, 65478096920, 981807)
+conta1.extrato()
+conta1.depositar(1000)
+conta1.sacar(500)
+conta2 = Conta("paninick",13694206667, 1000, 13694206667, 212009)
